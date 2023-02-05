2570765ab14d9cd5d60cd6fdc0e4ba47a33b0d4b
Jon Pacheco <jon.pacheco@tutanota.com>
2023-02-05 15:15:11 +0000

    chore: reorganize string matcher into separate files

---
+++ src/WinForms.AutoComplete/StringMatch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace WinForms.AutoComplete;
+
+// TODO: Use spans instead of substrings
+
+/// <summary>
+/// The result of a match, which are stored in the suggestion listbox.
+/// </summary>
+public class StringMatch
+{
+    /// <summary>
+    /// The original source
+    /// </summary>
+    public string Text { get; internal set; } = string.Empty;
+    /// <summary>
+    /// The source decomposed on match/non matches against the pattern
+    /// </summary>
+    public List<string> Segments { get; internal set; } = new List<string>();
+    /// <summary>
+    /// Is the first segment a match?
+    /// </summary>
+    public bool StartsOnMatch { get; internal set; }
+}
---
+++ src/WinForms.AutoComplete/StringMatchingMethod.cs
@@ -0,0 +1,11 @@
+﻿namespace WinForms.AutoComplete;
+
+/// <summary>
+/// Represents the different ways in which a string can be matched using <see cref="StringMatcher"/>.
+/// </summary>
+public enum StringMatchingMethod
+{
+    NoWildcards,
+    UseWildcards,
+    UseRegexs,
+}
